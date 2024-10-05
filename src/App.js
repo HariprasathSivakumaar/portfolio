8b8f683daf8ec70e5058635de93f8d71f9e93b4c
HariprasathSivakumaar <hariprasathsivakumaar@gmail.com>
2024-10-06 03:50:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
